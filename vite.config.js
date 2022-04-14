bd5819334f0bea7af462addaf970f1cc7e6136fa
DaYePython <yesongsheng@163.com>
2022-04-14 14:41:29 +0800

    chore(yarn): :wrench:切换包管理工具
    将yarn转成pnpm

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
+import AutoImport from "unplugin-auto-import/vite";
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
+        AutoImport({
+            imports: ["vue", "@vueuse/core"],
+            dts: true,
+        }),
         Components({
-            resolvers: [NaiveUiResolver()]
-        })],
+            resolvers: [NaiveUiResolver()],
+        }),
+    ],
+
     base: "./",
     server: {
-        port: 8979
+        port: 8979,
     },
-})
+});
