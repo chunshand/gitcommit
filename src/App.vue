e65b22e22648bbf88a3ac8a32948deca95ca82dc
DaYePython <yesongsheng@163.com>
2022-04-15 15:38:26 +0800

    fix(useDark): :bug:修复错误引入vueuse的useDark

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
 </template>
 <script setup lang="ts">
 import { darkTheme } from "naive-ui";
+import useDark from "./composeables/useDark";
 const isDark = useDark()
 const theme = computed<typeof darkTheme | null>(() => {
     return isDark.value ? darkTheme : null
