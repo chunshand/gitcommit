0934e3fb0bf205f365613ee3bd07e2a84cff87be
chunshan <1484082125@qq.com>
2023-06-19 00:22:44 +0800

    f

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
-    <NConfigProvider :theme="theme">
-        <NDialogProvider>
-            <NMessageProvider>
-                <Home />
-            </NMessageProvider>
-        </NDialogProvider>
-    </NConfigProvider>
+  <NConfigProvider :theme="theme">
+    <NDialogProvider>
+      <NMessageProvider>
+        <Home />
+      </NMessageProvider>
+    </NDialogProvider>
+  </NConfigProvider>
 </template>
 <script setup lang="ts">
 import { darkTheme, NDialogProvider } from "naive-ui";
 import useDark from "./composables/useDark";
 const isDark = useDark()
 const theme = computed<typeof darkTheme | null>(() => {
-    return isDark.value ? darkTheme : null
+  return isDark.value ? darkTheme : null
 })
 
 </script>
