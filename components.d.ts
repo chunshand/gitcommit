bb289875cebda0953a46bce5b1e463fe39238edb
chunshan <1484082125@qq.com>
2023-08-07 15:43:12 +0800

    合并pr

--- components.d.ts
+++ components.d.ts
@@ -6,20 +6,6 @@ declare module 'vue' {
   export interface GlobalComponents {
     EmojiLabel: typeof import('./src/components/EmojiLabel.vue')['default']
     Home: typeof import('./src/components/home.vue')['default']
-    NButton: typeof import('naive-ui')['NButton']
-    NCard: typeof import('naive-ui')['NCard']
-    NCheckbox: typeof import('naive-ui')['NCheckbox']
-    NConfigProvider: typeof import('naive-ui')['NConfigProvider']
-    NDrawer: typeof import('naive-ui')['NDrawer']
-    NDrawerContent: typeof import('naive-ui')['NDrawerContent']
-    NEllipsis: typeof import('naive-ui')['NEllipsis']
-    NGrid: typeof import('naive-ui')['NGrid']
-    NGridItem: typeof import('naive-ui')['NGridItem']
-    NInput: typeof import('naive-ui')['NInput']
-    NMessageProvider: typeof import('naive-ui')['NMessageProvider']
-    NSelect: typeof import('naive-ui')['NSelect']
-    NSpace: typeof import('naive-ui')['NSpace']
-    NText: typeof import('naive-ui')['NText']
     SettingsView: typeof import('./src/components/SettingsView.vue')['default']
   }
 }
