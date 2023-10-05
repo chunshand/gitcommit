65362bdadd72a6b48a7fc6e6f896b3f45255f6c6
xyeluo <yeluoyh@outlook.com>
2023-10-05 23:39:36 +0800

    feat: 🚸 自定义emoji对象有效性验证

--- src/components/CustomEmoji.vue
+++ src/components/CustomEmoji.vue
@@ -6,7 +6,7 @@
                     <NText depth="1">自定义emoji： </NText>
                     <NButton type="success" size="small" @click="saveEmojisData">保存</NButton>
                 </div>
-                <NInput v-model:value="userEmojis" type="textarea" rows="13" />
+                <NInput v-model:value="userEmojis" type="textarea" rows="13" :status="inputStatus" />
             </NSpace>
         </NDrawerContent>
     </NDrawer>
@@ -20,20 +20,51 @@ let { isOpenWindow, switchWindow: switchCustom } = useWindowHandle()
 isOpenWindow.value = true
 defineExpose({ switchCustom })
 
-const { error } = useMessage()
-const { updateEmojis, readCustomEmojis, saveCustomEmojis } = useEmojisStore()
+const { error, success } = useMessage()
+const { updateEmojis, customEmojis, saveCustomEmojis } = useEmojisStore()
 
-let userEmojis = ref(JSON.stringify(readCustomEmojis(), undefined, 2))
+let userEmojis = ref(JSON.stringify(customEmojis, undefined, 2))
+let inputStatus = ref<"success" | "error">("success")
+
+const customEorror = (str: string) => {
+    error(str, { duration: 10000, closable: true })
+    inputStatus.value = "error"
+}
+const customSuccess = (str: string) => {
+    success(str)
+    inputStatus.value = "success"
+}
+
+/**
+ * 验证自定义emoji的每个对象是否存在特定key
+ * @param emojisData
+ * @returns boolean 每个对象都存在keys时才返回true
+ */
+const validEmojiKey = (emojisData: RawEmoji[]) => {
+    const keys = ["name", "emoji"]
+    return !emojisData.some((emojiItem) => {
+        let flag = emojiItem.hasOwnProperty(keys[0]) ? false : keys[0]
+
+        flag = flag === false ? (emojiItem.hasOwnProperty(keys[1]) ? false : keys[1]) : flag
+
+        if (keys.includes(flag as string)) {
+            // 不存在flag键，终止遍历并返回true
+            customEorror(`[JSON Key Error]:对象中缺少${flag}键`)
+            return true
+        }
+        return flag
+    })
+}
 
 const saveEmojisData = () => {
     try {
-        let customEmojis: RawEmoji[] = JSON.parse(userEmojis.value)
-        updateEmojis(customEmojis)
-        saveCustomEmojis(customEmojis)
+        let emojisData: RawEmoji[] = JSON.parse(userEmojis.value)
+        if (!validEmojiKey(emojisData)) return
+        updateEmojis(emojisData)
+        saveCustomEmojis(emojisData)
+        customSuccess("保存成功")
     } catch (err) {
-        error(`[JSON Error]:${err}`, { duration: 0, closable: true })
+        customEorror(`[JSON Error]:${err}`)
     }
 }
 </script>
-
-<style scoped></style>
--- src/store/emojis.ts
+++ src/store/emojis.ts
@@ -2,6 +2,14 @@ import { useSearchTxtArr } from "@/composables/useSearch";
 import { type RawEmoji, rawEmojis } from "@/data";
 import EmojiLabel from "@/components/EmojiLabel.vue";
 
+const CUSTOM_EMOJIS_KEY = "CUSTOM_EMOJIS_STORE";
+
+const readCustomEmojis = () => {
+  const customEmoji: RawEmoji[] = utools.dbStorage.getItem(CUSTOM_EMOJIS_KEY);
+  if (customEmoji === null) return [];
+  return customEmoji;
+};
+
 const toEmojiOptions = (item: RawEmoji) => ({
   ...item,
   searchTxtArr: useSearchTxtArr(item),
@@ -17,27 +25,24 @@ const toEmojiOptions = (item: RawEmoji) => ({
 });
 
 export const useEmojisStore = createGlobalState(() => {
-  const CUSTOM_EMOJIS_KEY = "CUSTOM_EMOJIS_STORE";
+  const rawEmojiOptions = rawEmojis.map(toEmojiOptions);
 
-  const readCustomEmojis = () => {
-    const customEmoji: RawEmoji[] = utools.dbStorage.getItem(CUSTOM_EMOJIS_KEY);
-    if (customEmoji === null) return [];
-    return customEmoji;
-  };
-  rawEmojis.push(...readCustomEmojis());
-  let emojiOptions = ref(rawEmojis.map(toEmojiOptions));
-
+  let customEmojis = readCustomEmojis();
+  let emojiOptions = ref(
+    rawEmojiOptions.concat(...customEmojis.map(toEmojiOptions))
+  );
   /**
    * 除了增加新的emoji列表外，相同的emoji会覆盖已有的
-   * @param coustomEmojis
+   * @param customEmojis
    */
-  const updateEmojis = (coustomEmojis: RawEmoji[]) => {
+  const updateEmojis = (customEmojis: RawEmoji[]) => {
     type EmojiOptionsType = ReturnType<typeof toEmojiOptions>;
     let newEmojis: EmojiOptionsType[] = [];
     let result: number;
 
-    // todo: 已有的自定义emoji再保存会重新参与遍历，应该排除上一次自定义emoji
-    coustomEmojis.forEach((userEmoji) => {
+    emojiOptions.value = rawEmojis.map(toEmojiOptions);
+
+    customEmojis.forEach((userEmoji) => {
       result = emojiOptions.value.findIndex(
         (emoji) => emoji.name === userEmoji.name
       );
@@ -51,6 +56,7 @@ export const useEmojisStore = createGlobalState(() => {
       // 新的emoji会暂存在newEmojis，等自定义的全部遍历完在一起push
       else newEmojis.push(toEmojiOptions(userEmoji));
     });
+
     emojiOptions.value.push(...newEmojis);
   };
 
@@ -58,5 +64,5 @@ export const useEmojisStore = createGlobalState(() => {
     utools.dbStorage.setItem(CUSTOM_EMOJIS_KEY, customEmoji);
   };
 
-  return { emojiOptions, updateEmojis, readCustomEmojis, saveCustomEmojis };
+  return { emojiOptions, updateEmojis, customEmojis, saveCustomEmojis };
 });
